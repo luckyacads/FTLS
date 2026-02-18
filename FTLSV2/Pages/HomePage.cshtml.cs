5a47843de60593619947904cd60bb38d3ce8fc18
Pryde2322 <159122432+Pryde2322@users.noreply.github.com>
2026-02-18 16:23:06 +0800

    Created Homepage html with css, and renamed the index to LoginPage

---
+++ FTLSV2/Pages/HomePage.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FTLSV2.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        // This runs when the page first loads
+        public void OnGet()
+        {
+        }
+
+        // This will run when you click the "Finalize Assignment" button
+        public IActionResult OnPost()
+        {
+            // Logic for saving assignments goes here
+            return Page();
+        }
+    }
+}
