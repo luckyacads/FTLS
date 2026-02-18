a3bf5e7c4ca3924a024e3cecaae5ae74a9a18628
Pryde2322 <159122432+Pryde2322@users.noreply.github.com>
2026-02-18 22:17:59 +0800

    AdminPage HTML and Admin CSS

---
+++ FTLSV2/Pages/AdminPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FTLSV2.Pages
+{
+    public class AdminPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
