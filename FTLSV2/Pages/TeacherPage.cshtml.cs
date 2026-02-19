78bea77f5a44979acfad0ff848a25060dcf51069
Pryde2322 <159122432+Pryde2322@users.noreply.github.com>
2026-02-19 15:53:54 +0800

    Reapply "Created TeacherPage HTML and TeacherPage CSS"
    This reverts commit 1fcede2b6e82186f8a3d2cbd7b000bbd4b4244b5.

---
+++ FTLSV2/Pages/TeacherPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FTLSV2.Pages
+{
+    public class TeacherPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
