2024c305b9c32d0a78644c3d16e337a68d15cac8
harleybacalso <harleybacalso16@gmail.com>
2026-02-21 13:29:20 +0800

    edited the login page, renames homepage to chairmanpage, redirect admin login to adminpage, teacher login to teacherpage, chairman login to chairmanpage

--- FTLSV2/Pages/ChairmanPage.cshtml.cs
+++ FTLSV2/Pages/ChairmanPage.cshtml.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FTLSV2.Pages
 {
-    public class HomePageModel : PageModel
+    public class ChairmanPageModel : PageModel
     {
         // This runs when the page first loads
         public void OnGet()
         {
         }
 
-        // This will run when you click the "Finalize Assignment" button
+        // This will run when you click the "Finalize Assignment" button    
         public IActionResult OnPost()
         {
             // Logic for saving assignments goes here
